d30cf4a5950f8fd153368637c618af0ecc23ecef
Emmanuel Senno <senno.emmanuel92@gmail.com>
2024-07-26 14:33:40 +0100

    Segunda Version

--- appcoder/views.py
+++ appcoder/views.py
@@ -18,3 +18,14 @@ def profesores(request):
 def entregables(request):
     return render(request, "appcoder/entregables.html")
 
+from appcoder.models import Curso
+
+def curso_formulario(request):
+    if request.method == 'POST':
+        
+        curso = Curso(nombre=request.POST['curso'], camada=request.POST['camada'])
+        curso.save()
+        
+        return render(request, "appcoder/inicio.html")
+    
+    return render (request, "appcoder/curso_formulario.html")
